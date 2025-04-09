71a39bfb939b17ba4b2242bb33bb27de4d8a23f9
Anuj <anujtiwari4454@outlook.com>
2025-04-09 10:36:07 +0530

    add fix

--- server/routes/classRoutes.js
+++ server/routes/classRoutes.js
@@ -1,76 +1,28 @@
-const express = require("express");
-const { tokenMiddleware } = require("../middleware/authMiddleware");
-const { cacheMiddleware } = require("../middleware/cacheMiddleware");
-const { getUpcomingClasses } = require("../handlers/upcomingClassesHandler");
-const {
-  getTodayClasses,
-  getTomorrowClasses,
-  getDayAfterTomorrowClasses,
-} = require("../handlers/todayClassesHandler");
-const { handleError } = require("../utils/errorHandler");
+const express = require('express');
+const { tokenMiddleware } = require('../middleware/authMiddleware');
+const { cacheMiddleware } = require('../middleware/cacheMiddleware');
+const { getUpcomingClasses } = require('../handlers/upcomingClassesHandler');
+const { getTodayClasses } = require('../handlers/todayClassesHandler');
+const { handleError } = require('../utils/errorHandler');
 
 const router = express.Router();
 
-router.get(
-  "/upcoming-classes",
-  tokenMiddleware,
-  cacheMiddleware,
-  async (req, res) => {
-    try {
-      const upcomingClasses = await getUpcomingClasses(
-        req.headers["x-csrf-token"]
-      );
-      res.json(upcomingClasses);
-    } catch (error) {
-      handleError(res, error);
-    }
+router.get("/upcoming-classes", tokenMiddleware, cacheMiddleware, async (req, res) => {
+  try {
+    const upcomingClasses = await getUpcomingClasses(req.headers["x-csrf-token"]);
+    res.json(upcomingClasses);
+  } catch (error) {
+    handleError(res, error);
   }
-);
+});
 
-router.get(
-  "/today-classes",
-  tokenMiddleware,
-  cacheMiddleware,
-  async (req, res) => {
-    try {
-      const todayClasses = await getTodayClasses(req.headers["x-csrf-token"]);
-      res.json(todayClasses);
-    } catch (error) {
-      handleError(res, error);
-    }
+router.get("/today-classes", tokenMiddleware, cacheMiddleware, async (req, res) => {
+  try {
+    const todayClasses = await getTodayClasses(req.headers["x-csrf-token"]);
+    res.json(todayClasses);
+  } catch (error) {
+    handleError(res, error);
   }
-);
-
-router.get(
-  "/tomorrow-classes",
-  tokenMiddleware,
-  cacheMiddleware,
-  async (req, res) => {
-    try {
-      const tomorrowClasses = await getTomorrowClasses(
-        req.headers["x-csrf-token"]
-      );
-      res.json(tomorrowClasses);
-    } catch (error) {
-      handleError(res, error);
-    }
-  }
-);
-
-router.get(
-  "/day-after-tomorrow-classes",
-  tokenMiddleware,
-  cacheMiddleware,
-  async (req, res) => {
-    try {
-      const dayAfterTomorrowClasses = await getDayAfterTomorrowClasses(
-        req.headers["x-csrf-token"]
-      );
-      res.json(dayAfterTomorrowClasses);
-    } catch (error) {
-      handleError(res, error);
-    }
-  }
-);
+});
 
 module.exports = router;
