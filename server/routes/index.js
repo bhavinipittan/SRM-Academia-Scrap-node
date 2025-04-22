07ea5f4254de281d0fe5763cc3f548b133911b03
bhavinipittan <142975733+bhavinipittan@users.noreply.github.com>
2025-04-22 20:55:13 +0530

    index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -7,10 +7,17 @@ const combinedRoutes = require("./combinedRoutes");
 
 const router = express.Router();
 
+// Test route to confirm API is working
+router.get("/api/test", (req, res) => {
+  res.send("API is working!");
+});
+
+// Optional hello route
 router.get("/hello", (req, res) => {
   res.json({ message: "Hello, World!" });
 });
 
+// Use other route files
 router.use(authRoutes);
 router.use(classRoutes);
 router.use(academicRoutes);
